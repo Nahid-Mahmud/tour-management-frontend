f66c07ff50597e07bcceed7f002b297e0ac157ae
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-08-06 18:29:43 +0600

    feat: implement MainLayout with NavBar and Footer components
    - Added MainLayout component to structure the main application layout.
    - Introduced NavBar component for navigation links and branding.
    - Created Footer component for additional information and links.
    - Updated MainLayout to include NavBar at the top and Footer at the bottom.
    - Implemented responsive design for NavBar using Popover and NavigationMenu components.
    - Added logo component for branding in the NavBar.
    - Established utility components for navigation and popover functionalities.

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="grid min-h-screen grid-rows-[auto_1fr_auto]">
+      <NavBar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
