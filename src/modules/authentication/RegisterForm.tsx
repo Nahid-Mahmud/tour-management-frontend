df4331338128641fc6353d81afd42488e307339d
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-08-08 22:29:06 +0600

    feat: enhance RegisterForm with detailed validation and improved UI components

--- src/modules/authentication/RegisterForm.tsx
+++ src/modules/authentication/RegisterForm.tsx
@@ -2,18 +2,41 @@ import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router";
-import { zodResolver } from "@hookform/resolvers/zod";
 
 import { z } from "zod";
 
-const formSchema = z
+const registerSchema = z
   .object({
-    name: z.string().min(2).max(100).nonempty(),
+    name: z
+      .string()
+      .min(2, {
+        message: "Name must be at least 2 characters long",
+      })
+      .max(100)
+      .nonempty(),
     email: z.email(),
-    password: z.string().min(8).max(100).nonempty(),
-    confirmPassword: z.string().min(8).max(100).nonempty(),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long" })
+      .regex(/[A-Z]/, {
+        message: "Password must contain at least one uppercase letter",
+      })
+      .regex(/[a-z]/, {
+        message: "Password must contain at least one lowercase letter",
+      })
+      .regex(/[0-9]/, {
+        message: "Password must contain at least one number",
+      })
+      .regex(/[^A-Za-z0-9]/, {
+        message: "Password must contain at least one special character",
+      }),
+    confirmPassword: z.string().nonempty({
+      message: "Confirm Password is required",
+    }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
@@ -21,8 +44,10 @@ const formSchema = z
   });
 
 export function RegisterForm({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
   const form = useForm({
-    resolver: zodResolver(formSchema),
+    resolver: zodResolver(registerSchema),
     defaultValues: {
       name: "",
       email: "",
@@ -31,7 +56,7 @@ export function RegisterForm({ className, ...props }: React.HTMLAttributes<HTMLD
     },
   });
 
-  const onsubmit = (data) => {
+  const onsubmit = (data: z.infer<typeof registerSchema>) => {
     console.log("Form submitted with data:", data);
   };
 
@@ -44,7 +69,7 @@ export function RegisterForm({ className, ...props }: React.HTMLAttributes<HTMLD
 
       <div className="grid gap-6">
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onsubmit)}>
+          <form className="space-y-4" onSubmit={form.handleSubmit(onsubmit)}>
             <FormField
               control={form.control}
               name="name"
@@ -54,12 +79,104 @@ export function RegisterForm({ className, ...props }: React.HTMLAttributes<HTMLD
                   <FormControl>
                     <Input placeholder="John Doe" {...field} />
                   </FormControl>
-                  <FormDescription>This is your public display name.</FormDescription>
+                  <FormDescription className="sr-only">This is your public display name.</FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
             />{" "}
-            <Button type="submit">Submit</Button>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="john.doe@example.com" {...field} />
+                  </FormControl>
+                  <FormDescription className="sr-only">This is your email address.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />{" "}
+            <div className="relative">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="••••••••" type={showPassword ? "text" : "password"} {...field} />
+                    </FormControl>
+                    <FormDescription className="sr-only">This is your password.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />{" "}
+              <Button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className={`bg-transparent text-black hover:bg-transparent shadow-none absolute right-0 top-[1.4rem] cursor-pointer`}
+              >
+                {showPassword ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="lucide lucide-eye-icon lucide-eye"
+                  >
+                    <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
+                    <circle cx="12" cy="12" r="3" />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="lucide lucide-eye-closed-icon lucide-eye-closed"
+                  >
+                    <path d="m15 18-.722-3.25" />
+                    <path d="M2 8a10.645 10.645 0 0 0 20 0" />
+                    <path d="m20 15-1.726-2.05" />
+                    <path d="m4 15 1.726-2.05" />
+                    <path d="m9 18 .722-3.25" />
+                  </svg>
+                )}
+              </Button>
+            </div>
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="••••••••" type="password" {...field} />
+                  </FormControl>
+                  <FormDescription className="sr-only">This is your password confirmation.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />{" "}
+            <Button
+              type="submit"
+              className="w-full"
+              // disabled={!form.formState.isValid || form.formState.isSubmitting}
+            >
+              Submit
+            </Button>
           </form>
         </Form>
 
