cc47cf6282852fbfc705eb2575376d7b9d7c9dd9
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-08-13 18:13:03 +0600

    feat: add routes for Analytics and Bookings pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,10 @@
 import MainLayout from "@/components/layouts/MainLayout";
 import About from "@/pages/About";
+import Analytics from "@/pages/admin/Analytics";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
+import Bookings from "@/pages/user/Bookings";
 import Verify from "@/pages/Verify";
 
 import { createBrowserRouter } from "react-router";
@@ -22,6 +24,16 @@ const routes = createBrowserRouter([
       },
     ],
   },
+
+  {
+    path: "/admin",
+    children: [{ path: "analytics", element: <Analytics /> }],
+  },
+  {
+    path: "/user",
+    children: [{ path: "bookings", element: <Bookings /> }],
+  },
+
   {
     path: "/login",
     element: <Login />,
