170df77c94a68a4458a676164f6cc4ca68dd78ac
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-08-13 22:08:24 +0600

    feat: define user and auth interfaces for improved type safety

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,26 @@ export interface ISidebarItem {
     element: React.ReactNode;
   }[];
 }
+
+export type TRole = "SUPER_ADMIN" | "ADMIN" | "USER";
+
+export interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  picture: string;
+  role: string;
+  isDeleted: boolean;
+  isActive: string;
+  isVerified: boolean;
+  auths: Auth[];
+  createdAt: string;
+  updatedAt: string;
+  address: string;
+  phone: string;
+}
+
+export interface Auth {
+  provider: string;
+  providerId: string;
+}
